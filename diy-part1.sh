35867d3e68744b118132f6286faf0f19adc4f09c
mickeywaley <mickeywaley@gmail.com>
2021-11-07 17:35:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,19 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 如：修改默认 IP为192.168.123.1
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+# 删除原主题
+#rm -rf package/lean/luci-theme-argon
+# 添加新的主题
+git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
+#取消bootstrap为默认主题
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+# 删除默认密码
+#sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
+
+
+
+
+
